e6fdaacef465a8be7cb7dea0d10db1ceb80215d5
Mel Maki <melody.rackham@gmail.com>
2020-02-03 14:18:28 -0700

    fixing files

---
+++ api/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const server = express();
+const cors = require('cors');
+
+const reviews = require('./review_router');
+const stylists = require('./stylist_router');
+const customers = require('./customer_router');
+
+server.use(express.json());
+server.use(cors());
+
+server.use('/api/reviews', reviews);
+server.use('/api/stylists', stylists);
+server.use('/api/customers', customers);
+
+server.get('/', (req, res) => {
+  res.send('Waiting for content...!');
+});
+
+module.exports = server;
